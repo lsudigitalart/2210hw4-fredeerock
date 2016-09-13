aa574585b4697d256bcf8554db1623961084053b
Derick Ostrenko <dostrenko@gmail.com>
2016-09-13 11:51:40 -0500

    mouse interaction

--- sketch.js
+++ sketch.js
@@ -1,10 +1,24 @@
-
-
 function setup() {
-
-
+  createCanvas(640, 480);
+  background(0);
 }
 
 function draw() {
+  if(mouseX < width/2){
+    fill(255, 45, 32);
+    ellipse(width/2, height/2, 100);
+  }
 
+  if(mouseX > width/2) {
+    fill(0, 45, 232);
+    ellipse(width/2, height/2, 100);
+  }
+}
+
+function mouseReleased() {
+  background(0, 255, 0);
+}
+
+function mousePressed() {
+  background(255, 0, 0);
 }
