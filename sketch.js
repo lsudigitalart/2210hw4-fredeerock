d3cf82a4997491f0714fd1916a89a44dcc022da4
Derick Ostrenko <dostrenko@gmail.com>
2016-09-13 11:31:16 -0500

    stage 2

--- sketch.js
+++ sketch.js
@@ -1,21 +1,10 @@
-var v;
+
 
 function setup() {
-  v = 100;
-  createCanvas(640, 480);
-  noCursor();
-  smooth();
-  stroke(255);
-  // noStroke();
-  background(255, 0, 0);
+
 
 }
 
 function draw() {
-  var mouseDist = dist(mouseX, mouseY, pmouseX, pmouseY);
-  strokeWeight(10);
-  background(255, map(mouseDist, 0, 70, 0, 255), map(mouseDist, 0, 70, 0, 255)-mouseX, 5);
-  line(mouseX, mouseY, pmouseX, pmouseY);
-  // ellipse(mouseX, mouseY, 20);
-  println(mouseDist);
+
 }
