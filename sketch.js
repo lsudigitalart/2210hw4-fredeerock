add6a228dcb18371f78c95b6b9855bf256758c40
Derick Ostrenko <dostrenko@gmail.com>
2016-09-15 09:57:17 -0500

    base

--- sketch.js
+++ sketch.js
@@ -1,36 +1,22 @@
+var ex = 100;
+var ey = 100;
+var ed = 100;
+
 function setup() {
   createCanvas(640, 480);
-
 }
 
 function draw() {
   background(150);
-  // if(mouseX < width/2){
-  //   fill(255, 45, 32);
-  //   ellipse(width/2, height/2, 100);
-  // }
-  //
-  // if(mouseX > width/2) {
-  //   fill(0, 45, 232);
-  //   ellipse(width/2, height/2, 100);
-  // }
 
-  var mdist = dist(mouseX, mouseY, width/2, height/2);
+  var mdist = dist(mouseX, mouseY, ex, ey);
 
-  if (mdist < 100/2) {
+  if (mdist < ed/2) {
     fill(255);
   } else {
     fill(0);
   }
 
-  ellipse(width/2, height/2, 100);
+  ellipse(ex, ey, ed);
 
-}
-
-function mouseReleased() {
-  background(0, 255, 0);
-}
-
-function mousePressed() {
-  background(255, 0, 0);
 }
