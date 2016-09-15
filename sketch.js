05ae567be83a91b8c4ca4ebd05f687366cb08ed2
Derick Ostrenko <dostrenko@gmail.com>
2016-09-15 09:54:24 -0500

    collisions

--- sketch.js
+++ sketch.js
@@ -1,18 +1,37 @@
+var ed = 100;
+var ex = 150;
+var ey = 200;
+
+var rx = 300;
+var ry = ey;
+var rw = ed;
+var rh = ed;
+
 function setup() {
   createCanvas(640, 480);
-  background(0);
 }
 
 function draw() {
-  if(mouseX < width/2){
-    fill(255, 45, 32);
-    ellipse(width/2, height/2, 100);
-  }
+  background(100);
 
-  if(mouseX > width/2) {
-    fill(0, 45, 232);
-    ellipse(width/2, height/2, 100);
+  var mdist = dist(mouseX, mouseY, ex, ey);
+
+  if(mdist < ed/2){
+    fill(0);
+  } else {
+    fill(255);
   }
+  ellipse(ex, ey, ed);
+
+  if(mouseX > rx && mouseY < ry + rh && mouseX < rx + rw && mouseY > ry) {
+    fill(255, 0, 0);
+  } else {
+    fill(255);
+  }
+  rect(rx, ry, rw, rh);
+
+  println("mx:", mouseX, "my:", mouseY, rx, ry )
+  rectMode(CORNER);
 }
 
 function mouseReleased() {
