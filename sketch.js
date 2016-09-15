8a78161c3cf8f66b90358761ae485c557e200679
Derick Ostrenko <dostrenko@gmail.com>
2016-09-15 11:14:44 -0500

    not working

--- sketch.js
+++ sketch.js
@@ -1,12 +1,39 @@
-var doonce = 1;
+var ex1 = 100;
+var ey1 = 100;
+var cstate1 = 1;
+
+var ex2 = 100;
+var ey2 = 100;
+var cstate2 = 0;
 
 function setup() {
-
+  createCanvas(640, 480);
 }
 
 function draw() {
-  if(doonce == 1) {
-    println("hi");
-    doonce = 0;
+  background(100);
+
+  if(mouseIsPressed) {
+    if(cstate1 == 1) {
+      ex1 = mouseX;
+      ey1 = mouseY;
+      // cstate1 = 0;
+      cstate2 = 1;
+    }
+
+    if(cstate2 == 1) {
+      ex2 = mouseX;
+      ey2 = mouseY;
+      cstate2 = 0;
+    }
   }
+    if(cstate1 == 1) {
+      ellipse (ex1, ey1, 100);
+    }
+
+    if(cstate2 == 1) {
+      ellipse (ex2, ey2, 200);
+    }
+
+
 }
